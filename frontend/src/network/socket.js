f4dc1fe240ca7243b086e80f9c0601d73379ee2f
Maxime Hanarte <mhanarte@f6r11s8.clusters.42paris.fr>
2026-01-27 11:26:15 +0100

    ok

--- frontend/src/network/socket.js
+++ frontend/src/network/socket.js
@@ -1,5 +1,15 @@
+// export function connectWS() {
+//   const proto = window.location.protocol === "https:" ? "wss" : "ws";
+//   const ws = new WebSocket(`${proto}://${window.location.host}/ws`);
+//   return ws;
+// }
+
+import { io } from 'socket.io-client';
+
 export function connectWS() {
-  const proto = window.location.protocol === "https:" ? "wss" : "ws";
-  const ws = new WebSocket(`${proto}://${window.location.host}/ws`);
-  return ws;
+	const socket = io('http://localhost:8080', {  // URL complète vers nginx
+		path: '/ws'
+	});
+
+	return socket;
 }
