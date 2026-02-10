93b5d555a7494fdda8ab0ddd985c8f09962798f9
Zakaria Meliani <zmeliani@f6r13s6.clusters.42paris.fr>
2026-02-10 12:46:42 +0100

    middlewares: update auth, add attachPlayerId and pagination

--- api/src/middlewares/auth.js
+++ api/src/middlewares/auth.js
@@ -3,15 +3,17 @@ import jwt from "jsonwebtoken";
 // Verify the JSON Web Token send by the client
 export const checkAuthToken = (req, res, next) => {
 	const authHeader = req.headers.authorization;
-	if (!authHeader) return res.sendStatus(401);
+	if (!authHeader)
+		return res.sendStatus(401);
 
 	const token = authHeader.split(" ")[1];
-	if (!token) return res.sendStatus(401);
+	if (!token)
+		return res.sendStatus(401);
 
 	try {
 		req.user = jwt.verify(token, process.env.JWT_SECRET);
+		next();
 	} catch {
 		return res.sendStatus(401);
 	}
-	next();
 };
